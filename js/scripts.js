1547c1aac5164dec3b1bd23eee2c4fb91a6071eb
KupferJ <jorgekupfer@gmx.de>
2022-02-02 16:33:41 +0100

    wrapped array into IIFE
    added "add" and "getAll" function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 
-let pokemonList= [
-  {name:'Bulbasaur', height:2.04, type:['grass', 'poison'], weaknesses:['fire', 'psychich', 'flying', 'ice']},
-  {name:'Charmander', height:2, type:'fire', weaknesses:['water', 'ground', 'rock']},
-  {name:'Lugia', height:17.01, type:['psychic', 'flying'], weaknesses:['ghost', 'dark', 'electric', 'ice', 'rock']},
-  {name:'Mew', height:1.04, type:'psychic', weaknesses:['ghost', 'dark', 'bug']},
-  {name:'Bellsprout', height:2.04, type:['grass', 'poison'], weaknesses:['fire', 'psychic', 'flying', 'ice']},
-  {name:'Meowth', height:1.04, type:'normal', weaknesses:'fighting'},
-  {name:'Mimikyu', height:.08, type:['ghost', 'fairy'], weaknesses:['ghost', 'steel']}
-]
+// pokemonList wrappet into an IIFE
+let pokemonRepository = (function () {
+  let pokemonList= [
+    {name:'Bulbasaur', height:2.04, type:['grass', 'poison'], weaknesses:['fire', 'psychich', 'flying', 'ice']},
+    {name:'Charmander', height:2, type:'fire', weaknesses:['water', 'ground', 'rock']},
+    {name:'Lugia', height:17.01, type:['psychic', 'flying'], weaknesses:['ghost', 'dark', 'electric', 'ice', 'rock']},
+    {name:'Meowth', height:1.04, type:'normal', weaknesses:'fighting'},
+    {name:'Mimikyu', height:.08, type:['ghost', 'fairy'], weaknesses:['ghost', 'steel']}
+  ];
 
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 
-pokemonList.forEach (function(property) {
+// forEach loop that prints the list
+pokemonRepository.getAll().forEach (function(property) {
   if (property.height > 10) {
     document.write(`${property.name} [height: ${property.height}] - Wow, that's big!! <br> `)
   } else if(property.height < .1) {
@@ -20,14 +32,7 @@ pokemonList.forEach (function(property) {
   }
 });
 
-/*
-for(let i=0; i<pokemonList.length; i++){
-  // adds messages for cases in which a pokemon is extraordinarily big or small
-  if(pokemonList[i].height > 10) {
-    document.write(`${pokemonList[i].name} [height: ${pokemonList[i].height}] - Wow, that's big!! <br> `)
-  } else if(pokemonList[i].height < .1) {
-    document.write(`${pokemonList[i].name} [height: ${pokemonList[i].height}] - That's tiny! <br> `)
-  } else {
-    document.write(`${pokemonList[i].name} [height: ${pokemonList[i].height}]<br> `)
-  }
-}*/
+// testing the getAll and add functions (because it is declared below, the added 'jigglypuff' is updated in the console, but not the document)
+console.log(pokemonRepository.getAll());
+console.log(pokemonRepository.add({name:'Jigglypuff', height:1.08, type:['normal', 'fairy'], weaknesses:['steel', 'poison']}));
+console.log(pokemonRepository.getAll());
