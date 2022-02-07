c1176b9b22f624efa06203c168c1fb8891f297d5
KupferJ <jorgekupfer@gmx.de>
2022-02-07 15:15:52 +0100

    added API, polyfills, css styling
    added code to fetch data from an API (data will be logged in console when clicking a button)
    
    added 2 polyfills
    
    added a loading animation in js (styled in css)

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,107 @@
 
-// pokemonList wrapped into an IIFE
+// pokemonList API link
 let pokemonRepository = (function () {
-  let pokemonList= [
-    {name:'Bulbasaur', height:2.04, type:['grass', 'poison'], weaknesses:['fire', 'psychich', 'flying', 'ice']},
-    {name:'Charmander', height:2, type:'fire', weaknesses:['water', 'ground', 'rock']},
-    {name:'Lugia', height:17.01, type:['psychic', 'flying'], weaknesses:['ghost', 'dark', 'electric', 'ice', 'rock']},
-    {name:'Meowth', height:1.04, type:'normal', weaknesses:'fighting'},
-    {name:'Mimikyu', height:.08, type:['ghost', 'fairy'], weaknesses:['ghost', 'steel']}
-  ];
+  let pokemonList= [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'
+
 
   function add(pokemon) {
-    // validation to check if the parameter is an object
-      if(typeof pokemon === 'object'){
-    // validation to check whether all parameters are equal to the expected keys    
-      if (Object.keys(pokemon)[0] === 'name' &&
-          Object.keys(pokemon)[1] === 'height' &&
-          Object.keys(pokemon)[2] === 'type' &&
-          Object.keys(pokemon)[3] === 'weaknesses') {
+    // validation to check if the parameter is an object, has "name" in it
+      if (
+        typeof pokemon === "object" &&
+        "name" in pokemon) {
         pokemonList.push(pokemon);
-      }
+    } else {
+      console.log("error - parameters not fulfilled");
     }
-  }    
+  }
 
   function getAll() {
     return pokemonList;
   }
 
-  //created button for each Item of the pokemon list
+  // function to create button for each item on list
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
-   // Event Listener function to show details of the pokemon list
-    button.addEventListener('click', function() {
-      showDetails(pokemon);
-    })
     button.innerText = pokemon.name;
     button.classList.add('btn-class');
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
-    }
+    button.addEventListener("click", function(  ) {
+      showDetails(pokemon);
+    });
+  }
+  
+  // function to load the pokemon list from the API
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl).then(function (response) {
+      hideLoadingMessage();
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      hideLoadingMessage();
+      console.error(e);
+    });
+  }  
+
+  // function to load pokemon details from the API
+  function loadDetails(item) {
+    showLoadingMessage();
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      hideLoadingMessage();
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      hideLoadingMessage();
+      console.error(e);
+    });
+  }
+
+  // executed when user clicks on pokemon
+  function showDetails (item) {
+    loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+
+  // function to show animation when loading pokemon details
+  function showLoadingMessage() {
+    let loading = document.querySelector('#loading-spinner');
+    loading.classList.add('visibility');
+  }
+
+  // function to hide loading animation once loading is complete
+  function hideLoadingMessage() {
+    let loading = document.querySelector('#loading-spinner');
+    loading.classList.remove('visibility');
+  }
 
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
-//function to display details from the pokemon list in console, called with the event listener
-function showDetails(pokemon) {
-  console.log(pokemon);
-}
 
-
-// testing the getAll and add functions
-console.log(pokemonRepository.getAll());
-console.log(pokemonRepository.add({name:'Jigglypuff', height:1.08, type:['normal', 'fairy'], weaknesses:['steel', 'poison']}));
-console.log(pokemonRepository.getAll());
-
-
-
-pokemonRepository.getAll().forEach (function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
